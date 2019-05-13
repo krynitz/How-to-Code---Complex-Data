03a09d7336f24db92e79d85ce0c2a0fd39d211e3
tyson <tyson22509@hotmail.com>
2019-05-13 09:12:05 -0400

    Completed up to Week 8b

---
+++ Week 8a/Cross-Product.rkt
@@ -0,0 +1,51 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-abbr-reader.ss" "lang")((modname Cross-Product) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;; Data Definitions:
+
+;; ListOfString is one of:
+;; - empty
+;; - (cons String ListOfString)
+;; interp. list of string
+
+(define LOS0 empty)
+(define LOS1 (cons "a" empty))
+(define LOS2 (cons "a" (cons "b" empty)))
+(define LOS3 (cons "c" (cons "b" (cons "a" empty))))
+
+#;
+(define (fn-for-los los)
+  (cond [(empty? los) (...)]
+        [else
+         (... (first los)
+              (fn-for-los (rest los)))]))
+
+;; Function
+
+;; ListOfString ListOfString -> Boolean
+;; produce true if list is a prefix of second list
+(check-expect (prefix=? empty empty) true)
+(check-expect (prefix=? (list "x") empty) false)
+(check-expect (prefix=? empty (list "x")) true)
+(check-expect (prefix=? (list "x") (list "x")) true)
+(check-expect (prefix=? (list "x") (list "y")) false)
+(check-expect (prefix=? (list "x" "y") (list "x" "y")) true)
+(check-expect (prefix=? (list "x" "x") (list "x" "y")) false)
+(check-expect (prefix=? (list "x") (list "x" "y")) true)
+(check-expect (prefix=? (list "x" "y" "z") (list "x" "y")) false)
+
+; (define (prefix=? lsta lstb) false) ; stub
+
+#;
+(define (prefix=? lsta lstb)
+  (cond [(and (empty? lsta) (empty? lstb)) (...)]
+        [(and (cons? lsta) (empty? lstb)) (... lsta ...)]
+        [(and (empty? lsta) (cons? lstb)) (... lstb ...)]
+        [(and (cons? lsta) (cons? lstb)) (... lsta lstb)]))
+
+(define (prefix=? lsta lstb)
+  (cond [(empty? lsta) true]
+        [(empty? lstb) false]
+        [else (and (string=? (first lsta)
+                             (first lstb))
+                   (prefix=? (rest lsta) (rest lstb)))]))
